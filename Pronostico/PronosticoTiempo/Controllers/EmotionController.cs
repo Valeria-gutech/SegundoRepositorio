3bbf0d38c25af0b6bf56867b8f084dfe79b17f93
ValeriaGutierrez <ValeGe@DESKTOP-VESR4FH>
2021-02-25 18:13:41 -0400

    Emocion sin modelo

---
+++ Pronostico/PronosticoTiempo/Controllers/EmotionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PronosticoTiempo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmotionController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConNumero")]
+        public string Emocion(int suerte)
+        {
+            if (suerte == 0)
+                return "sos una persona diferente";
+            if (suerte == 1)
+                return "sos una persona feliz";
+            if (suerte == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado, no te puedo atender";
+
+        }
+        [HttpPost]
+        [Route("ConNumero")]
+        public string Emotion2([FromHeader] int suerte)
+        {
+            if (suerte == 0)
+                return "sos una persona diferente";
+            if (suerte == 1)
+                return "sos una persona feliz";
+            if (suerte == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado, no te puedo atender";
+
+        }
+    }
+}
